2ca8892fc6f3aa13e74482a503be1e96f1eca856
Nicolas Agustin Perez <nicolasperez95.a@gmail.com>
2024-08-17 13:24:42 -0300

    feat: first commit with hypper cutter

---
+++ Rpg-combat-system/Hypercutter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5.0f;
+}
